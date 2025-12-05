7bd6c6bc5ccdfa9c56f404de054af123eecff802
JosephTeo79 <hai2022my@gmail.com>
2025-12-05 13:29:41 +0800

    Update tab.js

--- js/tab.js
+++ js/tab.js
@@ -45,7 +45,7 @@ async function openTab(title, url) {
 
             for (let i = 1; i <= numPages; i++) {
                 const page = await pdf.getPage(i);
-                const viewport = page.getViewport({ scale: 1}); // 可以调大 scale
+                const viewport = page.getViewport({ scale: 3}); // 可以调大 scale
                 const canvas = document.createElement("canvas");
 
                 // 高分屏处理
