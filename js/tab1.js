f01ba64963101fd24fd9a95776a88bed31f5af3f
JosephTeo79 <hai2022my@gmail.com>
2025-12-08 16:09:03 +0800

    Change video width from 100% to 80%

--- js/tab1.js
+++ js/tab1.js
@@ -108,7 +108,7 @@ async function openResourceTab(title, resource) {
                 video.src = videoUrl;
                 video.controls = true;
                 video.setAttribute("controlsList", "nodownload");
-                video.style.width = "100%";
+                video.style.width = "80%";
                 video.style.height = "auto";
                 video.setAttribute("playsinline", "true");
                 video.addEventListener("contextmenu", e => e.preventDefault());
