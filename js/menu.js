76d08662287c27c15bcd5a1da7837b0c7812f772
JosephTeo79 <hai2022my@gmail.com>
2025-12-13 21:33:17 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,23 @@
-// menu.js
+// js/menu.js
 const menuData = [
-  { title: "Introduction", url: "introduction.html", icon: "icon/pdf.png" },
-  { 
-    title: "Front Office", 
-    icon: "icon/folder.jpg",
+  {
+    title: "Introduction",
+    url: "introduction.html"
+  },
+  {
+    title: "Front Office",
     children: [
-      { title: "POS Training Document", url: "FrontOffice/POS_Training_Document.pdf", icon: "icon/pdf.png" },
-      { title: "Login Logout", resource: "FrontOffice/Login_Logout", icon: "icon/pdf.png" },
-      { title: "Open Register", resource: "FrontOffice/Open_Register", icon: "icon/pdf.png" }
+      { title: "POS Training Document", url: "FrontOffice/POS_Training_Document.pdf" },
+      { title: "Login Logout", resource: "FrontOffice/Login_Logout" },
+      { title: "Open Register", resource: "FrontOffice/Open_Register" }
     ]
   },
-  { title: "Back Office", icon: "icon/folder.jpg", children: [] },
-  { title: "Support Handbook", url: "notebooklm.html", icon: "icon/NoteBooklm.png" }
+  {
+    title: "Back Office",
+    children: []
+  },
+  {
+    title: "Support Handbook",
+    url: "notebooklm.html"
+  }
 ];
